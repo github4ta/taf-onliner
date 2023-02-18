83654807f222315b1e2a5ec6ad0e2dc25b3a829f
AndreyPonomarev <Andrey.Ponomarev.it@gmail.com>
2023-02-19 00:55:06 +0300

    add onlinerTest class

--- src/main/java/by/itacademy/onliner/OnlinerPage.java
+++ src/main/java/by/itacademy/onliner/OnlinerPage.java
@@ -1,4 +1,5 @@
 package by.itacademy.onliner;
 
 public class OnlinerPage {
+    public static final String LOGIN_URL="https://Onliner.by";
 }
--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -1,4 +1,27 @@
 package by.itacademy.onliner;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerTest {
+    WebDriver driver;
+    @Test
+    public void testOpenOnliner() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.LOGIN_URL);
+        driver.quit();
+    }
+
+
+    @After
+    public void endTest() {
+
+        driver.quit();
+    }
 }
