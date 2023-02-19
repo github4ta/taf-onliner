f861bb984d5cfffad35d14a27acda1cc8bad9803
Sveta <skostyko@gmail.com>
2023-02-19 23:12:50 +0100

    refactor OnlinerTest, add testHousesAndFlats

--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -1,5 +1,6 @@
 package by.itacademy.onliner;
 
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,12 +25,20 @@ public class OnlinerTest {
     public void testOpenOnliner() {
 
     }
-    
+
     @Test
-    public void testCart(){
+    public void testCart() {
         driver.findElement(By.xpath(OnlinerPage.BUTTON_CART)).click();
         WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.LABEL_CART));
         Assertions.assertEquals("Корзина", TITLE_CART_Element.getText());
+    }
+
+    @Test
+    public void testHousesAndFlats() {
+        driver.findElement(By.xpath(OnlinerPage.LINK_HOUSES_AND_APARTMENTS)).click();
+        WebElement textOfBtnSale = driver.findElement(By.xpath(OnlinerPage.LABEL_SALE));
+        Assertions.assertEquals("Продажа", textOfBtnSale.getText());
+
     }
 
     @AfterEach
