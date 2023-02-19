083e5cf3512a88782cc948e1c1f6ed4df5f0bcce
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-19 16:56:38 +0300

    add static variables BTN_CART and TEXT_CART to OnlinerPage class add testCart to OnlinerTest1 class

--- src/main/java/by/itacademy/onliner/OnlinerPage.java
+++ src/main/java/by/itacademy/onliner/OnlinerPage.java
@@ -2,4 +2,7 @@ package by.itacademy.onliner;
 
 public class OnlinerPage {
     public static final String LOGIN_URL="https://Onliner.by";
+    public static final String BTN_CART = "//a[@title='Корзина']";
+    public static final String TITLE_CART = "//div[contains(text(), 'Корзина')]";
 }
+
--- src/test/java/by/itacademy/onliner/OnlinerTest1.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest1.java
@@ -1,8 +1,12 @@
 package by.itacademy.onliner;
 
+import jdk.jshell.execution.Util;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest1 {
@@ -14,11 +18,19 @@ public class OnlinerTest1 {
         driver.get(OnlinerPage1.URL);
         driver.quit();
     }
+    @Test
+    public void testCart(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.LOGIN_URL);
+        driver.findElement(By.xpath(OnlinerPage.BTN_CART)).click();
+        WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_CART));
+        Assert.assertEquals("Корзина", TITLE_CART_Element.getText());
+    }
 
 
     @After
     public void endTest() {
-
         driver.quit();
     }
 }
