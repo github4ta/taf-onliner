f2cd89cf83888ce7e327ee754127c7daf696caa2
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-19 17:50:44 +0300

    DELETE previous changes from branch "master" add static variables BTN_CART and TEXT_CART to OnlinerPage class add testCart to OnlinerTest class

--- src/main/java/by/itacademy/onliner/OnlinerPage.java
+++ src/main/java/by/itacademy/onliner/OnlinerPage.java
@@ -2,7 +2,5 @@ package by.itacademy.onliner;
 
 public class OnlinerPage {
     public static final String LOGIN_URL="https://Onliner.by";
-    public static final String BTN_CART = "//a[@title='Корзина']";
-    public static final String TITLE_CART = "//div[contains(text(), 'Корзина')]";
 }
 
--- src/test/java/by/itacademy/onliner/OnlinerTest1.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest1.java
@@ -18,17 +18,6 @@ public class OnlinerTest1 {
         driver.get(OnlinerPage1.URL);
         driver.quit();
     }
-    @Test
-    public void testCart(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnlinerPage.LOGIN_URL);
-        driver.findElement(By.xpath(OnlinerPage.BTN_CART)).click();
-        WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_CART));
-        Assert.assertEquals("Корзина", TITLE_CART_Element.getText());
-    }
-
-
     @After
     public void endTest() {
         driver.quit();
