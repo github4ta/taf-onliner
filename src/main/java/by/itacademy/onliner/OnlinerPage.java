5ab6242bf0e155775b51278efd02d396be44cb97
TatsianaZaiceva <tatsianazaiceva@gmail.com>
2023-02-19 17:46:59 +0300

    add brunch ONLINER-UI-151 add static variables BTN_CART and TEXT_CART to OnlinerPage class add testCart to OnlinerTest class

--- src/main/java/by/itacademy/onliner/OnlinerPage.java
+++ src/main/java/by/itacademy/onliner/OnlinerPage.java
@@ -1,4 +1,7 @@
 package by.itacademy.onliner;
 
 public class OnlinerPage {
+    public static final String LOGIN_URL="https://Onliner.by";
+    public static final String BTN_CART = "//a[@title='Корзина']";
+    public static final String TITLE_CART = "//div[contains(text(), 'Корзина')]";
 }
--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -1,4 +1,23 @@
 package by.itacademy.onliner;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerTest {
+    WebDriver driver;
+    @Test
+    public void testCart(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.LOGIN_URL);
+        driver.findElement(By.xpath(OnlinerPage.BTN_CART)).click();
+        WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_CART));
+        Assert.assertEquals("Корзина", TITLE_CART_Element.getText());
+        driver.quit();
+    }
 }
