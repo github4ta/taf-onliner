05a007d06b64d5487dc4a4a156bb5e95f6b421d7
Sergei Tsarik <serge.tsarik@gmail.com>
2023-02-19 19:28:19 +0300

    refactor code

--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -1,9 +1,12 @@
 package by.itacademy.onliner;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
@@ -24,10 +27,9 @@ public class OnlinerTest {
     
     @Test
     public void testCart(){
-        driver.findElement(By.xpath(OnlinerPage.BTN_CART)).click();
-        WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.TITLE_CART));
-        Assert.assertEquals("Корзина", TITLE_CART_Element.getText());
-        driver.quit();
+        driver.findElement(By.xpath(OnlinerPage.BUTTON_CART)).click();
+        WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.LABEL_CART));
+        Assertions.assertEquals("Корзина", TITLE_CART_Element.getText());
     }
 
     @AfterEach
