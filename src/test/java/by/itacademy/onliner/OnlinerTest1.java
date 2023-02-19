8bedffdcbf53156fc093e174933bf7d8c0d075dd
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-19 16:52:46 +0300

    add\refactor @AfterEach and @BeforEach

--- src/test/java/by/itacademy/onliner/OnlinerTest1.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest1.java
@@ -1,20 +1,27 @@
 package by.itacademy.onliner;
 
-import org.junit.After;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest1 {
-    ChromeDriver driver = new ChromeDriver();
-    @Test
-    public void testOpenOnliner() {
+    WebDriver driver;
+    @BeforeEach
+    public void warmUp( ) {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL_ONLINER);
     }
 
-    @After
-    public void endTest() {
+    @Test
+    public void testOpenOnliner(){
+
+    }
+
+    @AfterEach
+    public void tearDown() {
         driver.quit();
     }
 }
