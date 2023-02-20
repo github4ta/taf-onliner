cfd6cb4d3d21d54cbb74ea6fe267bac0acbd7cf1
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-20 12:21:10 +0300

    refactor OnlinerTest add testAbilitySearchByWord() method

--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -1,5 +1,6 @@
 package by.itacademy.onliner;
 
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 
 public class OnlinerTest {
 
@@ -30,6 +35,17 @@ public class OnlinerTest {
         driver.findElement(By.xpath(OnlinerPage.BUTTON_CART)).click();
         WebElement TITLE_CART_Element = driver.findElement(By.xpath(OnlinerPage.LABEL_CART));
         Assertions.assertEquals("Корзина", TITLE_CART_Element.getText());
+    }
+
+    @Test
+    public void testAbilitySearchByWord() {
+        driver.findElement(By.xpath(OnlinerPage.SEARCH_LINE)).sendKeys(OnlinerPage.TEXT_FOR_SEARCH);
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(OnlinerPage.SEARCH_FRAME)));
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(OnlinerPage.RESULT_SEARCH_FIRST_PRODUCT)));
+        String resultSearch = driver.findElement(By.xpath(OnlinerPage.RESULT_SEARCH_FIRST_PRODUCT)).getText();
+        Assert.isTrue(resultSearch.contains(OnlinerPage.TEXT_FOR_SEARCH), "Error");
     }
 
     @AfterEach
