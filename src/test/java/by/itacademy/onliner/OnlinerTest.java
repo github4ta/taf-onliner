7de4c2ff889ba3fc1aecbcc988add2bad14563f3
Sergei Tsarik <serge.tsarik@gmail.com>
2023-02-20 19:35:01 +0300

    refactor code

--- src/test/java/by/itacademy/onliner/OnlinerTest.java
+++ src/test/java/by/itacademy/onliner/OnlinerTest.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-import java.time.Duration;
-
 public class OnlinerTest {
 
     WebDriver driver;
