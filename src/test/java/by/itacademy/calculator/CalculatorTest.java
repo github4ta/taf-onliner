44a34116e5b84927a793804d14162328814dfb2f
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-02-24 21:00:16 +0300

    add driver

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -1,4 +1,11 @@
 package by.itacademy.calculator;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CalculatorTest {
+
+    public void testCalculator() {
+        WebDriver driver = new ChromeDriver();
+    }
 }
