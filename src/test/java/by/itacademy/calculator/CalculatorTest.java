9af7a0ec2784424c0179a7db33eb5cc2663adac0
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-24 21:54:58 +0300

    add calculatorStep object

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -14,16 +14,7 @@ public class CalculatorTest {
         driver.manage().window().maximize();
         String url = "https://svyatoslav.biz/testlab/wt/index.php";
         driver.navigate().to(url);
-
-        WebElement inputName = driver.findElement(By.xpath(CalculatorPage.INPUT_NAME));
-        inputName.sendKeys("Иван");
-        WebElement inputHeight = driver.findElement(By.xpath(CalculatorPage.INPUT_HEIGHT));
-        inputHeight.sendKeys("180");
-        WebElement inputWeight = driver.findElement(By.xpath(CalculatorPage.INPUT_WEIGHT));
-        inputWeight.sendKeys("78");
-        WebElement inputGender = driver.findElement(By.xpath(CalculatorPage.INPUT_GENDER_F));
-        inputGender.click();
-        WebElement buttonCalculate = driver.findElement(By.xpath(CalculatorPage.BUTTON_CALCULATE));
-        buttonCalculate.click();
+        CalculatorStep calculatorStep = new CalculatorStep();
+        calculatorStep.doCalculate();
     }
 }
