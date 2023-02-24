c9488276e1f4019286623bb6cfb2beabd337c835
Piotr Ropot <Ropot.Piotr@gmail.com>
2023-02-24 20:44:36 +0300

    add INPUT_GENDER_M

--- src/main/java/by/itacademy/calculator/CalculatorPage.java
+++ src/main/java/by/itacademy/calculator/CalculatorPage.java
@@ -5,4 +5,5 @@ public class CalculatorPage {
     static final String INPUT_NAME = "//input[@name='name']";
     static final String INPUT_HEIGHT = "//input[@name='height']";
     static final String INPUT_WEIGHT = "//input[@name='weight']";
+    public final String INPUT_GENDER_M = "//input[@value='m']";
 }
