8cc24703d964e81fbbf8ffea6386556fbb9f44b5
@NikName <@-zfBP8M*vkLhR6>
2023-02-24 20:39:38 +0300

    add INPUT_WEIGHT

--- src/main/java/by/itacademy/calculator/CalculatorPage.java
+++ src/main/java/by/itacademy/calculator/CalculatorPage.java
@@ -3,5 +3,6 @@ package by.itacademy.calculator;
 public class CalculatorPage {
 
     static final String INPUT_NAME = "//input[@name='name']";
-    static final String INPUT_HEIGHT="//input[@name='height']";
+    static final String INPUT_HEIGHT = "//input[@name='height']";
+    static final String INPUT_WEIGHT = "//input[@name='weight']";
 }
