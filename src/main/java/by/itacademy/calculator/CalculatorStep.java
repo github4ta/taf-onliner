923f0f2c4bb6821af90f28e086eb6c286c58125c
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-02-24 21:51:19 +0300

    add CalculatorStep

--- src/main/java/by/itacademy/calculator/CalculatorStep.java
+++ src/main/java/by/itacademy/calculator/CalculatorStep.java
@@ -1,20 +1,11 @@
 package by.itacademy.calculator;
 
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CalculatorTest {
+public class CalculatorStep {
 
-    @Test
-    public void testCalculator() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        String url = "https://svyatoslav.biz/testlab/wt/index.php";
-        driver.navigate().to(url);
-
+    public void doCalculate() {
         WebElement inputName = driver.findElement(By.xpath(CalculatorPage.INPUT_NAME));
         inputName.sendKeys("Иван");
         WebElement inputHeight = driver.findElement(By.xpath(CalculatorPage.INPUT_HEIGHT));
