a80e69e730b3826a0a823a50808710a64c7647da
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-24 14:02:40 -0500

    add driver to CalculatorStep

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
         driver.manage().window().maximize();
         String url = "https://svyatoslav.biz/testlab/wt/index.php";
         driver.navigate().to(url);
-        CalculatorStep calculatorStep = new CalculatorStep();
+        CalculatorStep calculatorStep = new CalculatorStep(driver);
         calculatorStep.doCalculate();
     }
 }
