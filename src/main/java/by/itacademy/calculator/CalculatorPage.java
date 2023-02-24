090102e8d19e1d8a83e55189c8ac09cd384cc8b9
Sveta <skostyko@gmail.com>
2023-02-24 18:48:37 +0100

    add INPUT_GENDER_F

--- src/main/java/by/itacademy/calculator/CalculatorPage.java
+++ src/main/java/by/itacademy/calculator/CalculatorPage.java
@@ -5,5 +5,6 @@ public class CalculatorPage {
     static final String INPUT_NAME = "//input[@name='name']";
     static final String INPUT_HEIGHT = "//input[@name='height']";
     static final String INPUT_WEIGHT = "//input[@name='weight']";
-    public final String INPUT_GENDER_M = "//input[@value='m']";
+    static final String INPUT_GENDER_M = "//input[@value='m']";
+    static final String INPUT_GENDER_F = "//input[@value='f']";
 }
