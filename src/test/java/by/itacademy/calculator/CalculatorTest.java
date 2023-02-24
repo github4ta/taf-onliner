651a7c9bdc9d35d056a69a3ad1f4912ffba00d73
Alina Darenskikh <alinadarenskikh@gmail.com>
2023-02-24 21:20:53 +0300

    add input_name.sendKeys

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package by.itacademy.calculator;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,11 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CalculatorTest {
 
+    @Test
     public void testCalculator() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         String url = "https://svyatoslav.biz/testlab/wt/index.php";
         driver.navigate().to(url);
         WebElement input_name = driver.findElement(By.xpath(CalculatorPage.INPUT_NAME));
+        input_name.sendKeys("Иван");
     }
 }
