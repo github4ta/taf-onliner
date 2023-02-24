73f35fd3048f695a35fc77e6af2c099ca6a3c17e
AndreyPonomarev <Andrey.Ponomarev.it@gmail.com>
2023-02-24 20:32:53 +0300

    add INPUT_HEIGHT

--- src/main/java/by/itacademy/calculator/CalculatorPage.java
+++ src/main/java/by/itacademy/calculator/CalculatorPage.java
@@ -3,4 +3,5 @@ package by.itacademy.calculator;
 public class CalculatorPage {
 
     static final String INPUT_NAME = "//input[@name='name']";
+    static final String INPUT_HEIGHT="//input[@name='height']";
 }
