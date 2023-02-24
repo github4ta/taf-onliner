908a0514cd428569cc8cd05d2f94545826ae639d
Pavel-VP-9 <pavelpetrik05241406@gmail.com>
2023-02-24 19:17:10 +0100

    add WebElement

--- src/test/java/by/itacademy/calculator/CalculatorTest.java
+++ src/test/java/by/itacademy/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package by.itacademy.calculator;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CalculatorTest {
@@ -10,5 +12,6 @@ public class CalculatorTest {
         driver.manage().window().maximize();
         String url = "https://svyatoslav.biz/testlab/wt/index.php";
         driver.navigate().to(url);
+        WebElement input_name = driver.findElement(By.xpath(CalculatorPage.INPUT_NAME));
     }
 }
