89f20cbc2a4276decab744858139612d82f6beb3
Sergei Tsarik <serge.tsarik@gmail.com>
2023-03-04 19:10:19 +0300

    add OnlinerStep

---
+++ src/main/java/by/itacademy/onliner/OnlinerStep.java
@@ -0,0 +1,12 @@
+package by.itacademy.onliner;
+
+import org.openqa.selenium.WebDriver;
+
+public class OnlinerStep {
+
+    WebDriver driver;
+
+    OnlinerStep(WebDriver driver) {
+        this.driver = driver;
+    }
+}
