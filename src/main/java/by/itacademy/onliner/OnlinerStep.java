d93d7455a8e3d120a8b957e3ad6386b1a9c39ee3
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-03-07 19:57:59 +0300

    add new method opentvsection in onlinerstep class snd refactor onlinertest

--- src/main/java/by/itacademy/onliner/OnlinerStep.java
+++ src/main/java/by/itacademy/onliner/OnlinerStep.java
@@ -29,4 +29,12 @@ public class OnlinerStep {
         WebElement labelForum = driver.findElement(By.xpath(OnlinerPage.LABEL_FORUM_TITLE));
         return labelForum.getText();
     }
+    public String openTVSection() {
+        driver.findElement(By.xpath((OnlinerPage.LINK_CATALOG))).click();
+        driver.findElement(By.xpath(OnlinerPage.BUTTON_CATALOG_ELECTRONICS)).click();
+        driver.findElement(By.xpath(OnlinerPage.LABEL_TV_AND_VIDEO)).click();
+        driver.findElement(By.xpath(OnlinerPage.CATALOG_NAVIGATION_LIST_ITEM_TV)).click();
+        WebElement labelTV = driver.findElement(By.xpath(OnlinerPage.LABEL_TV_TITLE));
+        return labelTV.getText();
+    }
 }
