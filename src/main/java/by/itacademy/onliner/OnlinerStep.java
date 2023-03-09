c3aa415fcaebfb294a5181011d50977d1d800286
Sveta <skostyko@gmail.com>
2023-03-09 18:36:00 +0100

    refactor OnlinerStep, add openHousesAndFlatsSection

--- src/main/java/by/itacademy/onliner/OnlinerStep.java
+++ src/main/java/by/itacademy/onliner/OnlinerStep.java
@@ -1,4 +1,24 @@
 package by.itacademy.onliner;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class OnlinerStep {
+    WebDriver driver;
+
+    OnlinerStep(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public List<WebElement> openHousesAndFlatsSection() {
+        driver.findElement(By.xpath(OnlinerPage.LINK_HOUSES_AND_APARTMENTS)).click();
+        driver.findElement(By.xpath(OnlinerPage.BUTTON_HOUSES_AND_APARTMENTS_SALE)).click();
+        List<WebElement> property = driver.findElements(By.xpath(OnlinerPage.LABEL_TITLE_SALE_AND_RENT));
+        System.out.println(property.get(0).getText());
+        System.out.println(property.get(1).getText());
+        return property;
+    }
 }
